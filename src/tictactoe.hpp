3253fc0d2a92ff6d0093dd6f212f5b675a08109a
tzuber-dev <zt50481@mail.harpercollege.edu>
2026-05-03 23:21:55 -0500

    Update tictactoe.hpp

--- src/tictactoe.hpp
+++ src/tictactoe.hpp
@@ -1,3 +1,59 @@
 #include <string>
 
-// Your functions go here
+
+
+class TTTMechanics {
+private:
+    //new code for portfolio 3
+    bool placedtrapcell;
+    bool activatedtrapcell;
+    int currenttrap;
+
+    // end code for portfolio 3
+    char board[9];
+    char currentplayerid;
+
+
+public:
+    TTTMechanics();
+
+    void resetgame();
+    void showboard() const;
+
+    char getplayer() const;
+    char retrievecell(int ind) const;
+
+    bool makemove(int pos);
+    bool checkifcelltaken(int pos) const;
+
+    bool winnerofgame() const;
+
+    bool draw() const;
+
+    void switchcurrentplayer();
+
+    //new code
+
+    int firstavailableselectionongrid() const;
+
+
+    //new code for portfolio 3
+
+
+    void activatetrapcellwhenever();
+    void deactivatetrapcell();
+    bool lastdecisionoftrapcell() const;
+    bool checkiftrapactivated() const;
+    int trapcelltest() const;
+    void placetrapcelltest(int var1);
+    int randomtrapcellselection() const;
+
+
+
+    //end code for portfolio 3
+
+
+
+
+
+};
